f9e260614f611f32b95878e1501e2bbb8f1ffaad
HaeGitHub <haekaltube1@gmail.com>
2022-03-30 20:15:25 +0700

    sedikit fix error di learn1.bash

--- learn1.bash
+++ learn1.bash
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-echo "Sekolah At-Taufiq"
-Name="Haekal"
-Job="Student
-
-# Ctrl + / untuk comments
-
-echo "Perkenalkan saya $Name"
-echo "berkerja sebagai $Job"
-
-read -p "Enter Your Name: " Read
-echo "$Read"
-
-
+echo "Sekolah AT"
+name="haekal"
+echo "my name is $name"
